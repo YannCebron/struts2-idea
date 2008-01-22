6733680def977a093e2480e4978d6384d84e5cbb
yann.cebron <yann.cebron@7711d44b-e23d-0410-aa76-77bfce44df11>
2008-01-22 20:54:36 +0000

    STRPL-41 plugin does not properly find action methods prefixed with get + test + completion-variants test

---
+++ plugin/testData/strutsXmlCompletion/src/ActionClass.java
@@ -0,0 +1,30 @@
+public class ActionClass {
+
+  private String myField;
+
+  public String validActionMethod() {
+    return null;
+  }
+
+  public String validActionMethodWithException() throws Exception {
+    return null;
+  }
+
+  public String getValidActionMethodNoUnderlyingField() {
+    return null;
+  }
+
+  // invalid action-method
+  public String getMyField() {
+    return myField;
+  }
+
+  public String invalidActionMethodDueToWrongExceptionType() throws IllegalStateException {
+    return null;
+  }
+
+  public boolean invalidActionMethodDueToWrongReturnType() throws Exception {
+    return false;
+  }
+
+}
--- plugin/testData/strutsXmlHighlighting/src/MyClass.java
+++ plugin/testData/strutsXmlHighlighting/src/MyClass.java
@@ -1,11 +1,22 @@
 public class MyClass {
 
+  private String myField;
+
   public String validActionMethod() {
     return null;
   }
 
   public String validActionMethodWithException() throws Exception {
     return null;
+  }
+
+  public String getValidActionMethodNoUnderlyingField() {
+    return null;
+  }
+
+  // invalid action-method
+  public String getMyField() {
+    return myField;
   }
 
   public String invalidActionMethodDueToWrongExceptionType() throws IllegalStateException {
