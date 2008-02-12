319bd8c301b0700d89563032a5e99dffc89dbd32
yann.cebron <yann.cebron@7711d44b-e23d-0410-aa76-77bfce44df11>
2008-02-12 20:29:42 +0000

    w/r to STRPL-13: - extract stuff to ActionUtil - tests

--- plugin/src/com/intellij/struts2/dom/struts/impl/ActionImpl.java
+++ plugin/src/com/intellij/struts2/dom/struts/impl/ActionImpl.java
@@ -15,10 +15,8 @@
 
 package com.intellij.struts2.dom.struts.impl;
 
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.*;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.util.PropertyUtil;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiMethod;
 import com.intellij.struts2.dom.struts.action.Action;
 import com.intellij.struts2.dom.struts.strutspackage.DefaultClassRef;
 import com.intellij.struts2.dom.struts.strutspackage.StrutsPackage;
@@ -27,10 +25,8 @@ import com.intellij.util.xml.GenericAttributeValue;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Pattern;
 
 /**
  * Adds utility methods.
@@ -45,17 +41,7 @@ public abstract class ActionImpl implements Action {
       return false;
     }
 
-    // do we have any wildcard-markers in our path? no --> exact compare
-    if (myPath.indexOf("*") == -1 &&
-        myPath.indexOf("**") == -1) {
-      return path.equals(myPath);
-    }
-
-    final String matchString = myPath.replaceAll("\\*", "\\\\S*"); // TODO exclude "/"
-    // TODO replace "**"
-    final boolean match = Pattern.matches(matchString, path);
-
-    return match;
+    return ActionUtil.matchesPath(myPath, path);
   }
 
   @NotNull
@@ -72,6 +58,7 @@ public abstract class ActionImpl implements Action {
     if (actionClassAttribute.getXmlElement() != null) {
       return actionClassAttribute.getValue();
     }
+    
     // resolve parent package <default-class-ref> (walk upwards)
     final DefaultClassRef ref = getStrutsPackage().searchDefaultClassRef();
     if (ref != null) {
@@ -111,66 +98,7 @@ public abstract class ActionImpl implements Action {
       return Collections.emptyList();
     }
 
-    final Project project = actionClass.getProject();
-    final PsiElementFactory psiElementFactory = PsiManager.getInstance(project).getElementFactory();
-    final GlobalSearchScope projectScope = GlobalSearchScope.allScope(project);
-    final PsiClassType stringType = psiElementFactory.createTypeByFQClassName(CommonClassNames.JAVA_LANG_STRING,
-                                                                              projectScope);
-    final PsiClassType exceptionType = psiElementFactory.createTypeByFQClassName("java.lang.Exception", projectScope);
-
-    final List<PsiMethod> actionMethods = new ArrayList<PsiMethod>();
-    for (final PsiMethod psiMethod : actionClass.getAllMethods()) {
-
-      if (psiMethod.isConstructor()) {
-        continue;
-      }
-
-      // only public non-static concrete methods
-      final PsiModifierList modifiers = psiMethod.getModifierList();
-      if (!modifiers.hasModifierProperty(PsiModifier.PUBLIC) ||
-          modifiers.hasModifierProperty(PsiModifier.STATIC) ||
-          modifiers.hasModifierProperty(PsiModifier.ABSTRACT)) {
-        continue;
-      }
-
-      // no parameters
-      if (psiMethod.getParameterList().getParametersCount() != 0) {
-        continue;
-      }
-
-      // skip "toString()"
-      if (psiMethod.getName().equals("toString")) {
-        continue;
-      }
-
-      // do not include simple getters (with underlying field)
-      if (PropertyUtil.isSimplePropertyGetter(psiMethod) &&
-          actionClass.findFieldByName(PropertyUtil.getPropertyName(psiMethod), true) != null) {
-        continue;
-      }
-
-      // only "throws java.lang.Exception" or no throws at all
-      final PsiClassType[] exceptionTypes = psiMethod.getThrowsList().getReferencedTypes();
-      if (exceptionTypes.length > 1) {
-        continue;
-      }
-
-      if (exceptionTypes.length == 1) {
-        if (!exceptionTypes[0].equals(exceptionType)) {
-          continue;
-        }
-      }
-
-      // return type "java.lang.String"
-      final PsiType type = psiMethod.getReturnType();
-      if (type != null &&
-          type instanceof PsiClassType &&
-          type.equals(stringType)) {
-        actionMethods.add(psiMethod);
-      }
-
-    }
-
-    return actionMethods;
+    return ActionUtil.findActionMethods(actionClass);
   }
+
 }
