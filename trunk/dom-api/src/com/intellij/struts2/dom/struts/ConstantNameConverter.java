c70c8bfd3387985df47d2bd0ba3ce810d7104c17
yann.cebron <yann.cebron@7711d44b-e23d-0410-aa76-77bfce44df11>
2008-06-22 13:03:13 +0000

    STRPL-65 Type-dependent autocompletion/validation for <constant> "value"

--- trunk/dom-api/src/com/intellij/struts2/dom/struts/ConstantNameConverter.java
+++ trunk/dom-api/src/com/intellij/struts2/dom/struts/ConstantNameConverter.java
@@ -19,61 +19,18 @@ import com.intellij.util.xml.ConvertContext;
 import com.intellij.util.xml.ResolvingConverter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * Provides the predefined <code>constant</code> names as completion variants.
- * <p/>
- * See <code>struts.properties</code> in struts.jar.
  *
  * @author Yann C&eacute;bron
  */
 public class ConstantNameConverter extends ResolvingConverter.StringConverter {
 
-  private static final List<String> PREDEFINED_VARIANTS =
-    Arrays.asList("struts.i18n.encoding",
-                  "struts.objectFactory",
-                  "struts.objectFactory.spring.autoWire",
-                  "struts.objectFactory.spring.useClassCache",
-                  "struts.objectTypeDeterminer",
-                  "struts.multipart.parser",
-                  "struts.multipart.saveDir",
-                  "struts.multipart.maxSize",
-                  "struts.custom.properties",
-                  "struts.mapper.class",
-                  "struts.action.extension",
-                  "struts.serve.static",
-                  "struts.serve.static.browserCache",
-                  "struts.enable.DynamicMethodInvocation",
-                  "struts.enable.SlashesInActionNames",
-                  "struts.tag.altSyntax",
-                  "struts.devMode",
-                  "struts.i18n.reload",
-                  "struts.ui.theme",
-                  "struts.ui.templateDir",
-                  "struts.ui.templateSuffix",
-                  "struts.configuration.xml.reload",
-                  "struts.velocity.configfile",
-                  "struts.velocity.contexts",
-                  "struts.velocity.toolboxlocation",
-                  "struts.url.http.port",
-                  "struts.url.https.port",
-                  "struts.url.includeParams",
-                  "struts.custom.i18n.resources",
-                  "struts.dispatcher.parametersWorkaround",
-                  "struts.freemarker.manager.classname",
-                  "struts.freemarker.templatesCache",
-                  "struts.freemarker.beanwrapperCache",
-                  "struts.freemarker.wrapper.altMap",
-                  "struts.xslt.nocache",
-                  "struts.configuration.files",
-                  "struts.mapper.alwaysSelectFullNamespace");
-
   @NotNull
   public Collection<? extends String> getVariants(final ConvertContext context) {
-    return PREDEFINED_VARIANTS;
+    return ConstantValueConverter.getConstantNames();
   }
 
 }
